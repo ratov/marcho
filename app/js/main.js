e024b3342c266700e7ffb2eda480c17a1b966566
Arthur Schopenhauer <lokigoldx8@gmail.com>
2022-05-23 21:00:20 +0300

    added contacts page

--- app/js/main.js
+++ app/js/main.js
@@ -72,49 +72,49 @@ $(function () {
 		starSvg: '<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="18px" height="16px" viewBox="0 0 18 16" version="1.1" ><g id="surface1"> <path d="M 11.914062 4.695312 L 16.402344 5.359375 C 16.773438 5.414062 17.085938 5.675781 17.207031 6.035156 C 17.324219 6.398438 17.226562 6.789062 16.960938 7.058594 L 13.703125 10.253906 L 14.472656 14.835938 C 14.535156 15.210938 14.382812 15.589844 14.070312 15.8125 C 13.757812 16.035156 13.351562 16.0625 13.015625 15.882812 L 9.003906 13.742188 L 4.992188 15.882812 C 4.65625 16.0625 4.246094 16.035156 3.9375 15.8125 C 3.628906 15.589844 3.472656 15.210938 3.539062 14.835938 L 4.304688 10.253906 L 1.050781 7.058594 C 0.78125 6.789062 0.683594 6.398438 0.804688 6.035156 C 0.921875 5.675781 1.230469 5.414062 1.605469 5.359375 L 6.09375 4.695312 L 8.105469 0.5625 C 8.273438 0.21875 8.621094 0 9.003906 0 C 9.386719 0 9.738281 0.21875 9.902344 0.5625 Z M 11.914062 4.695312 " /></g></svg> '
 	});
 
-//Секундомер
-function getTimeRemaining(endtime) {
-	const total = Date.parse(endtime) - Date.parse(new Date());
-	const seconds = Math.floor((total / 1000) % 60);
-	const minutes = Math.floor((total / 1000 / 60) % 60);
-	const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
-	const days = Math.floor(total / (1000 * 60 * 60 * 24));
+	//Секундомер
+	function getTimeRemaining(endtime) {
+		const total = Date.parse(endtime) - Date.parse(new Date());
+		const seconds = Math.floor((total / 1000) % 60);
+		const minutes = Math.floor((total / 1000 / 60) % 60);
+		const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+		const days = Math.floor(total / (1000 * 60 * 60 * 24));
 
-	return {
-		total,
-		days,
-		hours,
-		minutes,
-		seconds
-	};
-}
-
-function initializeClock(classWrapper, endtime) {
-	const clock = document.querySelector(classWrapper);
-	const daysSpan = clock.querySelector('.promo__days');
-	const hoursSpan = clock.querySelector('.promo__hours');
-	const minutesSpan = clock.querySelector('.promo__minutes');
-	const secondsSpan = clock.querySelector('.promo__seconds');
-
-	function updateClock() {
-		const t = getTimeRemaining(endtime);
-
-		daysSpan.innerHTML = t.days;
-		hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-		minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-		secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-
-		if (t.total <= 0) {
-			clearInterval(timeinterval);
-		}
+		return {
+			total,
+			days,
+			hours,
+			minutes,
+			seconds
+		};
 	}
 
-	updateClock();
-	const timeinterval = setInterval(updateClock, 1000);
-}
+	function initializeClock(classWrapper, endtime) {
+		const clock = document.querySelector(classWrapper);
+		const daysSpan = clock.querySelector('.promo__days');
+		const hoursSpan = clock.querySelector('.promo__hours');
+		const minutesSpan = clock.querySelector('.promo__minutes');
+		const secondsSpan = clock.querySelector('.promo__seconds');
 
-const deadline = $('.promo__clock').attr('data-time');
-// const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
-initializeClock('.promo__clock', deadline);
+		function updateClock() {
+			const t = getTimeRemaining(endtime);
+
+			daysSpan.innerHTML = t.days;
+			hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+			minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+			secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
+
+			if (t.total <= 0) {
+				clearInterval(timeinterval);
+			}
+		}
+
+		updateClock();
+		const timeinterval = setInterval(updateClock, 1000);
+	}
+
+	const deadline = $('.promo__clock').attr('data-time');
+	// const deadline = new Date(Date.parse(new Date()) + 15 * 24 * 60 * 60 * 1000);
+	initializeClock('.promo__clock', deadline);
 
 });
